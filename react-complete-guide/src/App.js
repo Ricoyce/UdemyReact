ed80899bc0c9108adb5094a4b986969db3baa581
Rico Otto <rico@2lemon.nl>
2019-12-07 22:58:47 +0100

    Rewrite class to a function component following the new React standard.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,18 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Person name="Rico" age="26" />
-        <Person name="Kim" age="26" />
-      </div>
-    );
-  }
+const app = props => {
+
+  return (
+    <div className="App">
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>And I am a developer.</Person>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+    </div>
+  );
 }
 
-export default App;
+export default app;
+
+state = {
+  persons: [
+    { name: 'Rico', age: 26 },
+    { name: 'Kim', age: 26 }
+  ],
+  otherState: 'some value'
+}
+
+switchNameHandler = () => {
+  this.setState({
+    persons: [
+      { name: 'Rico', age: 27 },
+      { name: 'Kim', age: 26 }
+    ]
+  })
+}
